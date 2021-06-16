9acd2876eaca697dc864088c84bc8ab751fff9fc
catenaprotocol <85307588+catenaprotocol@users.noreply.github.com>
2021-06-16 20:56:18 +0200

    Create faucet.sol
    Catena Faucet Smart Contract

---
+++ faucet.sol
@@ -0,0 +1,138 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-06-16
+*/
+
+pragma solidity ^0.6.12;
+// SPDX-License-Identifier: Unlicensed
+
+
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+interface ERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract Ownable is Context {
+    address private _owner;
+    address private _previousOwner;
+    uint256 private _lockTime;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+     /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+    function geUnlockTime() public view returns (uint256) {
+        return _lockTime;
+    }
+
+    //Locks the contract for owner for the amount of time provided
+    function lock(uint256 time) public virtual onlyOwner {
+        _previousOwner = _owner;
+        _owner = address(0);
+        _lockTime = now + time;
+        emit OwnershipTransferred(_owner, address(0));
+    }
+    
+    //Unlocks the contract for owner when _lockTime is exceeds
+    function unlock() public virtual {
+        require(_previousOwner == msg.sender, "You don't have permission to unlock");
+        require(now > _lockTime , "Contract is locked until 7 days");
+        emit OwnershipTransferred(_owner, _previousOwner);
+        _owner = _previousOwner;
+    }
+}
+
+contract Faucet is Ownable {
+    uint256 public _tokenAmount = 10 * 10**6 * 10**9;
+    uint256 public _waitTime = 60 minutes;
+
+    ERC20 public tokenInstance;
+    
+    mapping(address => uint256) lastAccessTime;
+
+    constructor(address _tokenInstance) public {
+        require(_tokenInstance != address(0));
+        tokenInstance = ERC20(_tokenInstance);
+    }
+
+    function requestTokens() public {
+        require(allowedToWithdraw(msg.sender));
+        tokenInstance.transfer(msg.sender, _tokenAmount);
+        lastAccessTime[msg.sender] = block.timestamp + _waitTime;
+    }
+
+    function allowedToWithdraw(address _address) public view returns (bool) {
+        if(lastAccessTime[_address] == 0) {
+            return true;
+        } else if(block.timestamp >= lastAccessTime[_address]) {
+            return true;
+        }
+        return false;
+    }
+    
+    function setTokenAmount(uint256 tokenAmount) external onlyOwner() {
+        _tokenAmount = tokenAmount;
+    }
+    
+    function setWaitTime(uint256 waitTime) external onlyOwner() {
+        _waitTime = waitTime;
+    }
+    
+    
+    
+}
